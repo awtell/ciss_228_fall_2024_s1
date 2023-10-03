4cb35a4dc90e20e55f06555978e4eb12f69f9cb9
charbelfakhry <53810858+charbelfakhry@users.noreply.github.com>
2023-10-03 14:02:48 +0300

    f

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -29,7 +29,7 @@ const insertUserController = async (req, res) =>
         || userPassword || userPassword !== ''
         )
     {
-        res.status(400).json({message: "missing data"});
+        return res.status(400).json({message: "missing data"});
     }
 
     try{
